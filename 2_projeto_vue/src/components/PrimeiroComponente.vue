f61e2fda6760f6bef5f58ed1926c29ef8185df04
Jamily <jamily751@hotmail.com>
2024-04-12 11:53:28 -0300

    criando componentes

---
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e eu sou {{ profissao }}</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'PrimeiroComponente',
+    data() {
+      return {
+        nome: 'Jamily',
+        profissao: 'enfermeira'
+      }
+    }
+  }
+
+</script>
